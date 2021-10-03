87a21fb27ae47db37b9aaefd9c8266008b5cd0f0
rpoublan <raphaelpoublan@gmail.com>
2021-10-03 18:54:59 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Appbar from './packages/appbar';
+
+const App = (): React.ReactElement => {
+  return (
+    <div className="App">
+      <Appbar />
+    </div>
+  );
+};
+
+export default App;
