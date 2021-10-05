72ed76cdd02e8394cf0deee34e160dd7b0c350a1
rpoublan <raphaelpoublan@gmail.com>
2021-10-05 23:05:15 +0200

    add first packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+import AppMenu from './packages/appmenu';
 import Appbar from './packages/appbar';
+import Breadcrumb from './packages/breadcrumbs';
 
 const App = (): React.ReactElement => {
   return (
     <div className="App">
       <Appbar />
+      <Grid container>
+        <Grid item xs={2}>
+          <AppMenu />
+        </Grid>
+        <Grid item xs={10} sx={{ padding: '16px' }}>
+          <Breadcrumb />
+        </Grid>
+      </Grid>
     </div>
   );
 };
---
+++ src/packages/categories/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Category = (): React.ReactElement => {
+  return <div>test</div>;
+};
+
+export default Category;
